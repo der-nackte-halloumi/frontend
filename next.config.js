f7d44ee058a0d277a6bec9345063263e738834a7
Christian Ost <hallo@christianost.de>
2020-02-21 14:53:31 +0100

    chore: add export command, add specific build folder

--- next.config.js
+++ next.config.js
@@ -4,5 +4,6 @@ module.exports = {
   env: {
     mapboxToken: process.env.FE_MAPBOX_TOKEN || envVariables.FE_MAPBOX_TOKEN,
     apiUrl: process.env.API_URL || envVariables.API_URL
-  }
+  },
+  distDir: "build"
 };
