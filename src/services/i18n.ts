0e0291871b4bc52c2d8a3d1411b9f24171f8a81c
Christian Ost <hallo@christianost.de>
2020-03-05 14:03:49 +0100

    feat: add browser language detector

--- src/services/i18n.ts
+++ src/services/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import de from '../translations/de.json';
 import en from '../translations/en.json';
@@ -11,11 +12,14 @@ const resources = {
   es: { translation: es },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  debug: process.env.NODE_ENV === 'development',
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'en',
+    fallbackLng: 'en',
+    debug: process.env.NODE_ENV === 'development',
+  });
 
 export default i18n;
