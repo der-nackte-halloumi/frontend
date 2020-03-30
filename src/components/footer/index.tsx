8d2fbb16a1073b360a0ad4ec798618df60567c75
Christian Ost <hallo@christianost.de>
2020-03-30 16:16:42 +0200

    fix: don't read from window when not defined

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -39,7 +39,7 @@ const Footer = ({ className }: HTMLAttributes<HTMLDivElement>): JSX.Element => {
           </a>
         </p>
         <p>
-          2020 – <a href="/">${window.location.host}</a>
+          2020 - <a href="/">${window?.location.host}</a>
         </p>
       </BaseFooter>
     </div>
