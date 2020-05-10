f1fd48b1827834b485bb57edb17c8dce6d195694
Christian Ost <hallo@christianost.de>
2020-05-10 23:08:56 +0200

    refactor: extract typewriter animation into own component to avoid rerendering of entire page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, InputHTMLAttributes } from 'react';
 import { styled } from 'linaria/react';
 import { useTranslation } from 'react-i18next';
 
@@ -22,48 +22,67 @@ const QuestionWrapper = styled.div`
     font-size: 1.5em;
     margin: 8px;
   }
+`;
 
-  input {
-    height: 2em;
-    color: #414141;
-    font-size: 1.5em;
-    text-align: center;
-    width: 100%;
-    background: #f2f2f2;
-    border: none;
-    border-top: 2px solid transparent;
-    border-bottom: 2px solid #727272;
-    padding: 4px;
-    transition: all 150ms;
-  }
+const Input = styled.input`
+  height: 2em;
+  color: #414141;
+  font-size: 1.5em;
+  text-align: center;
+  width: 100%;
+  background: #f2f2f2;
+  border: none;
+  border-top: 2px solid transparent;
+  border-bottom: 2px solid #727272;
+  padding: 4px;
+  transition: all 150ms;
 
-  input:hover {
+  &:hover {
     border-bottom-color: #424242;
   }
-
-  input:focus,
-  input:active {
+  &:focus,
+  &:active {
     color: #1d1e62;
     border: 2px solid #1d1e62;
     border-radius: 1em;
   }
 `;
 
+interface TypewriterInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  isFocused: boolean;
+  t: (key: string) => string;
+}
+const TypewriterInput = ({
+  isFocused,
+  t,
+  ...props
+}: TypewriterInputProps): JSX.Element => {
+  const placeholder = useTypewriterAnimation(isFocused ? [] : [...products], {
+    randomize: true,
+  });
+  return (
+    <Input
+      type="search"
+      role="searchbox"
+      aria-label={t('pages.index.search-label')}
+      placeholder={placeholder}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    />
+  );
+};
+
 const Home = (): JSX.Element => {
   const [query, setQuery] = useState('');
   const [location, setLocation] = useState(DEFAULT_LOCATION);
   const [isFocused, setIsFocused] = useState(false);
-  const placeholder = useTypewriterAnimation(isFocused ? [] : [...products], {
-    randomize: true,
-  });
   const debouncedSearchTerm = useDebounce(query, 300);
   const [shops, setShops] = useState<Array<Shop>>([]);
   const { t } = useTranslation();
 
   useInitialQueryValueFromRouter('q', (q) => {
-    if (!Array.isArray(q)) {
-      setQuery(q);
-    }
+    if (Array.isArray(q)) return;
+    setQuery(q);
   });
 
   useEffect(() => {
@@ -80,15 +99,13 @@ const Home = (): JSX.Element => {
       <Header />
       <QuestionWrapper>
         <p>{t('pages.index.search-1')}</p>
-        <input
-          type="search"
+        <TypewriterInput
           onChange={(event) => setQuery(event.currentTarget.value)}
           value={query}
-          role="searchbox"
-          aria-label={t('pages.index.search-label')}
-          placeholder={placeholder}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
+          isFocused={isFocused}
+          t={t}
         />
         <p>{t('pages.index.search-2')}</p>
       </QuestionWrapper>
